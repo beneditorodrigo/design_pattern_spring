eca8d0f4b090d7bc80407df08ea4f09651240628
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-10-26 13:41:16 -0300

    Resolvendo bugs na camada Service

--- src/main/java/com/beneditorodrigo/gof/service/ClienteService.java
+++ src/main/java/com/beneditorodrigo/gof/service/ClienteService.java
@@ -2,6 +2,14 @@ package com.beneditorodrigo.gof.service;
 
 import com.beneditorodrigo.gof.model.Cliente;
 
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ *
+ * @author beneditorodrigo
+ */
+
 public interface ClienteService {
 
     Iterable<Cliente> buscarTodos();
--- src/main/java/com/beneditorodrigo/gof/service/implement/ClienteServiceImplement.java
+++ src/main/java/com/beneditorodrigo/gof/service/implement/ClienteServiceImplement.java
@@ -1,10 +1,16 @@
 package com.beneditorodrigo.gof.service.implement;
 
 import com.beneditorodrigo.gof.model.Cliente;
+import com.beneditorodrigo.gof.model.ClienteRepository;
 import com.beneditorodrigo.gof.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ClienteServiceImplement implements ClienteService {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
     @Override
     public Iterable<Cliente> buscarTodos() {
         return null;
