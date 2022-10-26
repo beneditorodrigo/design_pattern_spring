9122b38bd83386e9bb9b09214aa47af656a2b5ce
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-10-26 11:13:18 -0300

    Adcionando camada Service

---
+++ src/main/java/com/beneditorodrigo/gof/service/ClienteService.java
@@ -0,0 +1,16 @@
+package com.beneditorodrigo.gof.service;
+
+import com.beneditorodrigo.gof.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+}
---
+++ src/main/java/com/beneditorodrigo/gof/service/implement/ClienteServiceImplement.java
@@ -0,0 +1,32 @@
+package com.beneditorodrigo.gof.service.implement;
+
+import com.beneditorodrigo.gof.model.Cliente;
+import com.beneditorodrigo.gof.service.ClienteService;
+
+public class ClienteServiceImplement implements ClienteService {
+
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
